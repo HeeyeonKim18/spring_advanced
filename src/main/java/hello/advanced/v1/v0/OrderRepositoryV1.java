2aa3d068614c3b058a35b0ef51f1b7f80770dcdb
Heeyeon Kim <go1927hm@gmail.com>
2024-06-16 22:20:37 +0900

    apply logs v1

---
+++ src/main/java/hello/advanced/v1/v0/OrderRepositoryV1.java
@@ -0,0 +1,36 @@
+package hello.advanced.v1.v0;
+
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.hellotrace.HelloTraceV1;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV1 {
+
+    private final HelloTraceV1 trace;
+
+    public void save(String itemId) throws IllegalAccessException, InterruptedException {
+
+        TraceStatus status = null;
+        try{
+            status = trace.begin("OrderRepository.save()");
+            if(itemId.equals("ex")){
+                throw new IllegalAccessException("예외 발생!");
+            }
+            sleep(1000);
+            trace.end(status);
+        }catch(Exception e){
+            trace.exception(status, e);
+            throw e; // 예외를 꼭 다시 던져주어야 한다.
+        }
+
+        // 저장 로직
+
+    }
+
+    private void sleep(int millis) throws InterruptedException {
+        Thread.sleep(millis);
+    }
+}
